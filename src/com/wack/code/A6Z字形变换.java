314d0755cda722bed0089c92f931e628dbeaff31
chriswack <wackliuchang@qq.com>
2019-07-26 12:02:10 +0800

    A6

--- src/com/wack/code/A6Z字形变换.java
+++ src/com/wack/code/A6Z字形变换.java
@@ -7,11 +7,12 @@ public class A6Z字形变换 {
         System.out.println(res);
         res = new A6Z字形变换().convert1("LDREOEIIECIHNTSG", 4);
         System.out.println(res);
-        System.out.println(Math.ceil(5 / 2));
+        res = new A6Z字形变换().convert2("LDREOEIIECIHNTSG", 4);
+        System.out.println(res);
     }
 
     public String convert(String s, int numRows) {
-        if (numRows <= 1) return s;
+        if (numRows == 1) return s;
 
         int rowLen = 2 * numRows - 2;
         int colLen = (int) Math.round(Math.ceil(s.length() * 1.0 / rowLen));
@@ -44,8 +45,24 @@ public class A6Z字形变换 {
     }
 
 
+    public String convert2(String s, int numRows) {
+        if (numRows == 1) return s;
+        StringBuilder sb = new StringBuilder();
+        int len = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col + row < len; col += cycleLen) {
+                sb.append(s.charAt(col + row));
+                if (row != 0 && row != numRows - 1 && col + cycleLen - row < len)
+                    sb.append(s.charAt(col + cycleLen - row));
+            }
+        }
+        return sb.toString();
+    }
+
     public String convert1(String s, int numRows) {
-        if (numRows <= 1) return s;
+        if (numRows == 1) return s;
         int rowLen = 2 * numRows - 2;
         int colLen = (int) Math.round(Math.ceil(s.length() * 1.0 / rowLen));
         StringBuilder sb = new StringBuilder();
@@ -68,4 +85,6 @@ public class A6Z字形变换 {
         }
         return sb.toString();
     }
+
+
 }
