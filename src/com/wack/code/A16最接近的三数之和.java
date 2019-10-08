29ebbf1d56b71867e0976dc798692e3a2eba5bad
chriswack <wackliuchang@qq.com>
2019-10-08 18:02:09 +0800

    A16

---
+++ src/com/wack/code/A16最接近的三数之和.java
@@ -0,0 +1,57 @@
+package com.wack.code;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * New
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ */
+public class A16最接近的三数之和 {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        int res = new A16最接近的三数之和().threeSumClosest(new int[]{0,2,1,-3}, 1);
+        System.out.println(res);
+        System.out.println(System.currentTimeMillis() - start + "ms");
+
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.length;i++) {
+            int start = i+1, end = nums.length - 1;
+            while(start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+                if(Math.abs(target - sum) < Math.abs(target - ans))
+                    ans = sum;
+                if(sum > target)
+                    end--;
+                else if(sum < target)
+                    start++;
+                else
+                    return ans;
+            }
+        }
+        return ans;
+    }
+
+
+}
